1c3a1f192e4539a5e62837a44cf5d092130486a0
ChinmayaR23 <94908427+ChinmayaR23@users.noreply.github.com>
2024-01-02 19:24:55 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement('h1', {}, 'Hello world from React');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+console.log(heading)
