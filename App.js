df4e69a12335ae715326a6dbebccc53405e78e92
ChinmayaR23 <94908427+ChinmayaR23@users.noreply.github.com>
2024-01-03 10:35:06 +0530

    ep02

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 const heading = React.createElement('h1', {}, 'Hello world from React');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
