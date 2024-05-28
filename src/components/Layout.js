a3d53e288ea98e948a5e3168f83c33e5892237f6
ChinmayaR23 <94908427+ChinmayaR23@users.noreply.github.com>
2024-05-28 19:34:32 +0530

    Fixed some issues in routing

---
+++ src/components/Layout.js
@@ -0,0 +1,15 @@
+// Layout.js
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
