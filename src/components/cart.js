65ef36107d3f7b9fa88d9c795625785152b3f7cc
Chinmaya R <94908427+ChinmayaR23@users.noreply.github.com>
2024-07-16 15:08:00 +0530

    Delete src directory

--- src/components/cart.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Cart = ()=>{
-    return(
-        <h1>This is your cart</h1>
-    )
-}
-
-export default Cart;
--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-
-import image from "./CR FOODS.png"
-
-
-export const LOGO_URL = image;
-export const IMG_SRC = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660";
