2cef3b279eca05c67f949711dc135c1f8f16e96d
Chinmaya R <94908427+ChinmayaR23@users.noreply.github.com>
2024-07-18 15:05:25 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <section className="relative bg-black text-center text-white py-4">
       <span className="font-bold">© 2023 CraveBite. All rights reserved.</span>
       <div className="flex justify-center">
-        <Link to={"https://github.com/Manoj-Kumar-Munda/CraveBite"} target="_blank">
+        <Link to={"https://github.com/ChinmayaR23"} target="_blank">
           <img src={github} alt="" className="w-12 h-12" title="Github" />
         </Link>
       </div>
